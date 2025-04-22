f28eb0f3211291c169fea9c7fad1b23f55bf037e
cypriankanda <cyprianboit@gmail.com>
2025-04-22 09:59:26 +0300

    PlantPage.js with update/delete handlers

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    setPlants([...plants, newPlant]);
+  };
+
+  const handleUpdatePlant = (updatedPlant) => {
+    setPlants(plants.map(p => (p.id === updatedPlant.id ? updatedPlant : p)));
+  };
+
+  const handleDeletePlant = (deletedId) => {
+    setPlants(plants.filter(p => p.id !== deletedId));
+  };
+
+  const filteredPlants = plants.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={setSearch} />
+      <PlantList
+        plants={filteredPlants}
+        onUpdatePlant={handleUpdatePlant}
+        onDeletePlant={handleDeletePlant}
+      />
     </main>
   );
 }
